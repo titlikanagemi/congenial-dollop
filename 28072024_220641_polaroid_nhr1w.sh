959d924f799aa11c85395e545876ae2df9c1421d
titlikanagemi <zviadkostava53@gmail.com>
2024-07-28 22:06:41 +0000

    Changing polaroid

---
+++ 28072024_220641_polaroid_nhr1w.sh
@@ -0,0 +1,1 @@
+Cricket chirping can tell the temperature outside. Counting how many times a cricket chirps in 15 seconds and then adding 40 to that number will approximately tell you what the temperature is in Fahrenheit
