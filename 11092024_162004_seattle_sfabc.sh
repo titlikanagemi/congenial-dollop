40af7b77bb0c036e7e021cf2824e652a34b3e6d0
titlikanagemi <zviadkostava53@gmail.com>
2024-09-11 16:20:04 +0000

    Updating seattle

---
+++ 11092024_162004_seattle_sfabc.sh
@@ -0,0 +1,1 @@
+Marlboro was the first cigarette company to market a cigarette that had a red filter called "beauty tip." This was done to hide the lipstick marks left on the filter from women smokers
