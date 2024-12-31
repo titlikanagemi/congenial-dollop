102a5cf5df977d6c8a37a24a5def5582ca71a631
titlikanagemi <zviadkostava53@gmail.com>
2024-12-31 20:52:01 +0000

    Setting myers

---
+++ 31122024_205201_myers_ewxab.sh
@@ -0,0 +1,1 @@
+The first jigsaw puzzle was created by map maker John Splisbury who mounted one of his maps on a sheet of hardwood. He proceeded to cut around the borders of each country use a fine saw
